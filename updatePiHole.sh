88980e39ea67832d474d88f81341134da9336f20
losgehts <losgehts22@web.de>
2020-08-26 21:17:55 +0200

    first version of file updatePiHole.sh

---
+++ updatePiHole.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# download filtered SV (RPiList) blacklists
+repo="https://raw.githubusercontent.com/losgehts/SVpihole-lists/master/blacklists/"
+curl -s -L "${repo}Corona-Blocklist" "${repo}Fake-Science" "${repo}Phishing-Angriffe" "${repo}Streaming " "${repo}Win10Telemetry" "${repo}child-protection" "${repo}crypto" "${repo}easylist" "${repo}gambling" "${repo}malware" "${repo}notserious" "${repo}samsung" "${repo}spam.mails" > filteredSVblacklist
+mv filteredSVblacklist /var/www/html/filteredSVblacklist
+
+# von Kuketz
+# Download AdBlock Lists (EasyList, EasyPrivacy, Fanboy Annoyance / Social Blocking)
+curl -s -L https://easylist.to/easylist/easylist.txt https://easylist.to/easylist/easyprivacy.txt https://easylist.to/easylist/fanboy-annoyance.txt https://easylist.to/easylist/fanboy-social.txt > adblock_tmp.unsorted
+
+# Look for: ||domain.tld^
+sort -u adblock_tmp.unsorted | grep ^\|\|.*\^$ | grep -v \/ > adblock_tmp.sorted
+
+# Remove extra chars and put list under lighttpd web root
+sed 's/[\|^]//g' < adblock_tmp.sorted > /var/www/html/adblock.sorted
+
+
+# Remove files we no longer need
+rm adblock_tmp.unsorted adblock_tmp.sorted
+
+
+# update PiHole
+pihole -g
